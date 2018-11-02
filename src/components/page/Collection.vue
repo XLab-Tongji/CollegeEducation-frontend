9e538cf72770b02c6e37cec0cac9fa335ec1370f
Techevan <techevanwang@outlook.com>
2018-11-02 21:14:01 +0800

    resolve conflicts

--- src/components/page/Collection.vue
+++ src/components/page/Collection.vue
@@ -99,21 +99,25 @@
     .el-row {
         margin-bottom: 20px;
     }
+
     .grid-content {
         display: flex;
         align-items: center;
         height: 100px;
     }
+
     .grid-cont-right {
         flex: 1;
         text-align: center;
         font-size: 14px;
         color: #999;
     }
+
     .grid-num {
         font-size: 30px;
         font-weight: bold;
     }
+
     .grid-con-icon {
         font-size: 50px;
         width: 100px;
@@ -122,24 +126,31 @@
         line-height: 100px;
         color: #fff;
     }
+
     .grid-con-1 .grid-con-icon {
         background: rgb(45, 140, 240);
     }
+
     .grid-con-1 .grid-num {
         color: rgb(45, 140, 240);
     }
+
     .grid-con-2 .grid-con-icon {
         background: rgb(100, 213, 114);
     }
+
     .grid-con-2 .grid-num {
         color: rgb(45, 140, 240);
     }
+
     .grid-con-3 .grid-con-icon {
         background: rgb(242, 94, 67);
     }
+
     .grid-con-3 .grid-num {
         color: rgb(242, 94, 67);
     }
+
     .user-info {
         display: flex;
         align-items: center;
@@ -147,41 +158,51 @@
         border-bottom: 2px solid #ccc;
         margin-bottom: 20px;
     }
+
     .user-avator {
         width: 120px;
         height: 120px;
         border-radius: 50%;
     }
+
     .user-info-cont {
         padding-left: 50px;
         flex: 1;
         font-size: 14px;
         color: #999;
     }
+
     .user-info-cont div:first-child {
         font-size: 30px;
         color: #222;
     }
+
     .user-info-list {
         font-size: 14px;
         color: #999;
         line-height: 25px;
     }
+
     .user-info-list span {
         margin-left: 70px;
     }
+
     .mgb20 {
         margin-bottom: 20px;
     }
+
     .todo-item {
         font-size: 14px;
     }
+
     .todo-item-del {
         text-decoration: line-through;
         color: #999;
     }
+
     .schart {
         width: 100%;
         height: 300px;
     }
+
 </style>
