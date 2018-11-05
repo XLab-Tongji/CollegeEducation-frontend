ff86ccb4b1fe32b75cb5f14b79b351d27d6cd3a1
maomao381 <512054675@qq.com>
2018-11-05 23:50:54 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -37,14 +37,36 @@ router.beforeEach((to, from, next) => {
     }
 })
 
+Date.prototype.format = function(fmt)
+{
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "H+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt))
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+    return fmt;
+}
+
 new Vue({
     router,
     render: h => h(App)
 }).$mount('#app');
-
+/*
 Vue.http.options.root='/article/all';
 Vue.http.post.root='/draft/save';
 Vue.http.post.root='/blackboard/save';
 Vue.http.post.root='/article/save';
+Vue.http.post.root='/article/reply';
+Vue.http.post.root='/article/like';
+Vue.http.post.root='/article/collect';
 Vue.http.headers.common['Authorization']='Bearer '+localStorage.getItem('token');
-
+*/
